a6b5b66e8c633cc0ca5cc2913cb377cecfdc3d37
kundan <goswamikundan123@gmail.com>
2023-02-26 11:36:03 +0530

    Adding file which comapres 3 strings using CompareTo method and displays max.

---
+++ Maximumstring.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximumGenerics
+{
+    public class Maximumstring
+    {
+        public static string MaximumString(string a, string b, string c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 || a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 || a.CompareTo(b) > 0 && a.CompareTo(c) >= 0)
+            {
+                return a;
+            }
+
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 || b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 || b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
+            {
+                return b;
+            }
+
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0 || c.CompareTo(a) >= 0 && c.CompareTo(b) > 0 || c.CompareTo(a) > 0 && c.CompareTo(b) >= 0)
+            {
+                return c;
+
+            }
+            return a;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,16 +6,31 @@
         {
             Console.WriteLine("Welcome to Test Maximum using Generics Program.");
 
-            Console.WriteLine("Maximum using CompareTo() Method");
-            Console.WriteLine("Enter 1st Number: ");
-            double a = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter 2nd Number: ");
-            double b = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter 3rd Number: ");
-            double c = Convert.ToDouble(Console.ReadLine());
-            
-            double max = MaximumFloat.MaximumFloatNumber(a, b, c);
-            Console.WriteLine("The maximum of the given three numbers is : " + max);
+            //Console.WriteLine("Maximum using CompareTo() Method");
+            //Console.WriteLine("Enter 1st Number: ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter 2nd Number: ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter 3rd Number: ");
+            //double c = Convert.ToDouble(Console.ReadLine());
+
+            //double max = MaximumFloat.MaximumFloatNumber(a, b, c);
+            //Console.WriteLine("The maximum of the given three numbers is : " + max);
+
+
+            Console.WriteLine("Maximum using CompareTo() Mathod");
+            Console.WriteLine("Enter 1st String: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Enter 2nd String: ");
+            string b = Console.ReadLine();
+            Console.WriteLine("Enter 3rd String: ");
+            string c = Console.ReadLine();
+
+
+
+            string max = Maximumstring.MaximumString(a, b, c);
+
+            Console.WriteLine("The maximum of the given three String is : " + max);
         }
     }
 }
