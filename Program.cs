a392276f29d3982be272519f27adf4bf224e385f
kundan <goswamikundan123@gmail.com>
2023-02-26 10:35:46 +0530

    Added: Greeting message to master Branch.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestMaximumGenerics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Test Maximum using Generics Program.");
+        }
+    }
+}
